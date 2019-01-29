90521871f39995aed7eafcfd9a251675c7b8bc9a
Hoàng Phạm Ngọc <pnhoang.bk@gmail.com>
2019-01-29 09:28:56 +0700

    update p2

--- replaceDigitWithPrime.rb
+++ replaceDigitWithPrime.rb
@@ -1,11 +1,9 @@
 def replaceDigitWithPrime n
     p = [2,3,5,7,11,13,17,19,23,29]
-	c = 0
 	t = 0	
     h = n.to_s.split('').reverse
     h.length.times do |i|
-		t += p[h[i].to_i] * 10**c
-        c += 1
+		t += p[h[i].to_i] * 10**i
     end
     t
 end
